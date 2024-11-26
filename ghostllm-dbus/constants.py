a1f1cf1dc8f5e955f8e49cae995d9cb1360fb7fd
Surya Teja K <shanmukhateja@gmail.com>
2024-11-26 20:00:34 +0530

    fix(ollama): use SYSTEM prompt to set instructions

--- ghostllm-dbus/constants.py
+++ ghostllm-dbus/constants.py
@@ -1,2 +1,7 @@
+# DBus
 DBUS_ADDRESS_NAME = 'in.suryatejak.ghostllm'
 DBUS_INTERFACE_NAME = '/in/suryatejak/ghostllm'
+
+# Ollama
+OLLAMA_MODEL_NAME='qwen2.5:3b'
+OLLAMA_SYSTEM_PROMPT='Your work is to rewrite sentences to make them more professional. Reply with 1 rewritten sentence. do not say anything else.'
--- ghostllm-dbus/ollama.py
+++ ghostllm-dbus/ollama.py
@@ -1,17 +1,16 @@
-from ollama import chat
-from ollama import ChatResponse
+from ollama import generate
+from ollama import GenerateResponse
+
+from .constants import OLLAMA_MODEL_NAME, OLLAMA_SYSTEM_PROMPT
 
 class OllamaController():
-    
+
     def chat(self, input_str: str) -> str:
-        response: ChatResponse = chat(
-            model='llama3.2',
-            messages=[
-                {
-                    'role': 'user',
-                    'content': f'Rewrite the input string "{input_str}". I only need single sentence. don\'t reply with anything else',
-                },
-            ]
+        response: GenerateResponse = generate(
+            model=OLLAMA_MODEL_NAME,
+            system=OLLAMA_SYSTEM_PROMPT,
+            stream=False,
+            prompt= f'rewrite "{input_str}"'
         )
 
-        return response.message.content
+        return response.response
